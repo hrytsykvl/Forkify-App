d5cce74647d7683450a7bf923f0ef3439c65fe64
hrytsykvl <gricikvlad@gmail.com>
2024-02-12 15:18:47 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4bfdb5c5-e4ff-4940-9959-15a48ffee952';
+export const MODAL_CLOSE_SEC = 2.5;
